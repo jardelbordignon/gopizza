60a7337a894c7b6a0e509d4f7b5d7aa3eb0e7e51
Jardel <jardel1101@gmail.com>
2022-04-16 11:42:57 -0300

    Ajuste InputPrice para funcionar com o hook form

--- app/src/components/InputPrice/index.tsx
+++ app/src/components/InputPrice/index.tsx
@@ -1,21 +1,52 @@
 import React from 'react'
+import { Control, Controller, FieldError } from 'react-hook-form'
 import { TextInputProps } from 'react-native'
 
 import * as S from './styles'
 
-type Props = TextInputProps & {
+type InputProps = TextInputProps & {
+  name: string
+  control: Control<any, any>
+  error?: FieldError
+  variant?: S.VariantType
   size: string
 }
 
-export const InputPrice = ({ size, ...rest }: Props) => {
+export const InputPrice = ({
+  size,
+  name,
+  control,
+  error,
+  variant = 'primary',
+  ...rest
+}: InputProps) => {
   return (
     <S.Wrapper>
-      <S.Size>
-        <S.Label>{size}</S.Label>
-      </S.Size>
+      <S.InputGroup variant={variant} isErrored={!!error}>
+        <Controller
+          control={control}
+          name={name}
+          render={({ field: { onChange, value, onBlur } }) => (
+            <>
+              <S.Size>
+                <S.Label>{size}</S.Label>
+              </S.Size>
 
-      <S.Label>R$</S.Label>
-      <S.Input keyboardType="numeric" {...rest} />
+              <S.Label>R$</S.Label>
+              <S.Input
+                keyboardType="numeric"
+                value={value}
+                onBlur={onBlur}
+                onChangeText={text => onChange(text)}
+                {...rest}
+              />
+            </>
+          )}
+        />
+      </S.InputGroup>
+      {error && (
+        <S.ErrorMessage variant={variant}>{error.message}</S.ErrorMessage>
+      )}
     </S.Wrapper>
   )
 }
--- app/src/components/InputPrice/styles.ts
+++ app/src/components/InputPrice/styles.ts
@@ -1,12 +1,32 @@
 import { TextInput } from 'react-native'
 import styled, { css } from 'styled-components/native'
 
+export type VariantType = 'primary' | 'secondary'
+
+type InputProps = {
+  variant?: VariantType
+  isFocused?: boolean
+  isErrored?: boolean
+  isFilled?: boolean
+}
+
 export const Wrapper = styled.View`
+  margin-bottom: 16px;
+`
+
+export const InputGroup = styled.View<InputProps>`
   width: 100%;
   height: 56px;
-  border: 1px solid ${({ theme }) => theme.COLORS.SHAPE};
+  border: 1px solid
+    ${({ theme, isErrored, variant }) =>
+      theme.COLORS[
+        isErrored
+          ? variant === 'secondary'
+            ? 'PRIMARY_50'
+            : 'PRIMARY_800'
+          : 'SHAPE'
+      ]};
   border-radius: 12px;
-  margin-bottom: 8px;
   flex-direction: row;
   align-items: center;
 `
@@ -32,4 +52,13 @@ export const Label = styled.Text`
 export const Input = styled(TextInput)`
   flex: 1;
   margin-left: 7px;
+`
+
+export const ErrorMessage = styled.Text<InputProps>`
+  ${({ theme, variant }) => css`
+    font-family: ${theme.FONTS.TEXT};
+    font-style: italic;
+    color: ${theme.COLORS[variant === 'secondary' ? 'TITLE' : 'PRIMARY_800']};
+    margin-left: 15px;
+  `}
 `
