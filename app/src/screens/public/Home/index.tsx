903cd2e0652d1a3a4f70ab134129ecd951ef4e3b
Jardel <jardel1101@gmail.com>
2022-04-17 05:26:37 -0300

    Incluindo InputSearch e ProductCard na Home

--- app/src/screens/public/Home/index.tsx
+++ app/src/screens/public/Home/index.tsx
@@ -4,6 +4,7 @@ import Icons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { useTheme } from 'styled-components/native'
 
 import happyEmoji from 'src/assets/happy.png'
+import { InputSearch, ProductCard } from 'src/components'
 
 import * as S from './styles'
 
@@ -22,6 +23,26 @@ export const Home = () => {
           <Icons name="logout" color={COLORS.TITLE} size={24} />
         </TouchableOpacity>
       </S.Header>
+
+      <InputSearch
+        placeholder="Pesquisar"
+        onSearch={() => {}}
+        onClear={() => {}}
+      />
+
+      <S.MenuHeader>
+        <S.Title>Cardápio</S.Title>
+        <S.MenuItemsNumber>123 pizzas</S.MenuItemsNumber>
+      </S.MenuHeader>
+
+      <ProductCard
+        product={{
+          id: 'abc',
+          name: 'Pizza de Batata',
+          description: 'Uma descrição',
+          imageUrl: 'https://github.com/jardelbordignon.png',
+        }}
+      />
     </S.Wrapper>
   )
 }
--- app/src/screens/public/Home/styles.ts
+++ app/src/screens/public/Home/styles.ts
@@ -15,7 +15,7 @@ export const Header = styled(LinearGradient).attrs(({ theme }) => ({
   justify-content: space-between;
   width: 100%;
 
-  padding: ${getStatusBarHeight() + 33}px 20px 24px;
+  padding: ${getStatusBarHeight() + 33}px 20px 56px;
 `
 
 export const Greeting = styled.View`
@@ -34,5 +34,32 @@ export const GreetingText = styled.Text`
   ${({ theme }) => css`
     font-family: ${theme.FONTS.TITLE};
     color: ${theme.COLORS.TITLE};
+  `}
+`
+
+export const MenuHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 25px 24px 0;
+  padding-bottom: 22px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${({ theme }) => theme.COLORS.SHAPE};
+`
+
+export const MenuItemsNumber = styled.Text`
+  font-size: 14px;
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TEXT};
+    color: ${theme.COLORS.SECONDARY_900};
+  `}
+`
+
+export const Title = styled.Text`
+  font-size: 20px;
+  line-height: 20px;
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TITLE};
+    color: ${theme.COLORS.SECONDARY_900};
   `}
 `
