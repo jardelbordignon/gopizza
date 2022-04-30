c748876b66290aeb5b9c31eda1d0f463a4335d32
Jardel <jardel1101@gmail.com>
2022-04-30 17:09:57 -0300

    Ajuste na forma passar o parâmetro rounded

--- app/src/components/Picture/index.tsx
+++ app/src/components/Picture/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import * as S from './styles'
 
@@ -6,24 +6,24 @@ type PictureProps = S.PictureStyleProps & {
   uri?: string | null
 }
 
-export const Picture = ({ uri, size, format }: PictureProps) => {
+export const Picture = ({ uri, size, rounded }: PictureProps) => {
   const [valid, setValid] = useState(true)
 
-  useEffect(() => setValid(true), [uri])
+  // useEffect(() => setValid(true), [uri])
 
   if (typeof uri === 'string' && valid) {
     return (
       <S.Image
         source={{ uri }}
         size={size}
-        format={format}
+        rounded={rounded}
         onError={() => setValid(false)}
       />
     )
   }
 
   return (
-    <S.PlaceholderWrapper size={size} format={format}>
+    <S.PlaceholderWrapper size={size} rounded={rounded}>
       <S.Placeholder>Nenhuma foto{'\n'}carregada</S.Placeholder>
     </S.PlaceholderWrapper>
   )
--- app/src/components/Picture/styles.ts
+++ app/src/components/Picture/styles.ts
@@ -2,24 +2,24 @@ import styled, { css } from 'styled-components/native'
 
 export type PictureStyleProps = {
   size?: string
-  format?: 'square' | 'rounded'
+  rounded?: boolean
 }
 
 export const Image = styled.Image<PictureStyleProps>`
-  ${({ size, format }) => css`
+  ${({ size, rounded }) => css`
     width: ${size || '100px'};
     height: ${size || '100px'};
-    border-radius: ${format === 'rounded' ? size || '100px' : '10px'};
+    border-radius: ${rounded ? size || '100px' : '10px'};
   `}
 `
 export const PlaceholderWrapper = styled.View<PictureStyleProps>`
   justify-content: center;
   align-items: center;
 
-  ${({ theme, size, format }) => css`
+  ${({ theme, size, rounded }) => css`
     width: ${size || '100px'};
     height: ${size || '100px'};
-    border-radius: ${format === 'rounded' ? size || '100px' : '10px'};
+    border-radius: ${rounded ? size || '100px' : '10px'};
     border: 1px dashed ${theme.COLORS.ALERT_900};
   `}
 `
